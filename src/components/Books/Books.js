d09856c4d52034d71a474773f2e6fccc19fdfe2e
brok3turtl3@gmail.com <62915047+brok3turtl3@users.noreply.github.com>
2022-11-30 19:53:13 -0500

    Landing page progress

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -4,7 +4,10 @@ import './books.css';
 
 const Books = () => {
   return (
-    <div>Books</div>
+    <div className="books-container">
+      <h1>BOOKS</h1>
+    <div><img src="./images/aaa.png" className="weight-chart" alt="jhgjg" /></div>
+    </div>
   )
 }
 
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -5,32 +5,36 @@ import './landing.css'
 const Landing = () => {
   return (
     <div className='landing-container'>
-        <setion className="question-section">
-<div className="question-container">
-    <p className="t-large mb1 p1">The Question: Is 50 too late to make significant life changes?</p>
+        <setion className="question-section mt2">
+<div className="question-container mt1 ml1">
+    <p className="t-large mb1 p1"><span className='t-large bold'>The Question:</span> Is 50 too late to make significant life changes?</p>
     <hr></hr>
     <p className='t-normal mt1 p1'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris malesuada augue id rutrum mollis. Morbi ut odio in tellus laoreet dignissim. Morbi ut erat in sem pharetra scelerisque maximus eu ante. Quisque accumsan urna pretium dolor iaculis finibus. Aliquam erat volutpat.</p>
 </div>
-<div className="stats-container">
+<div className="stats-container p1">
     <ul className="t-large">
-        <li className="t-large mb1">50% of ....</li>
-        <li className="t-large mb1">28% of ....</li>
-        <li className="t-large">1% of</li>
+        <li className="t-medium mb1 p1">The average Canadian watches 21 hours of TV a week.</li>
+        <li className="t-medium mb1 p1">The average Canadian generates 5.4lbs of garbage a day.</li>
+        <li className="t-medium p1">Over 99% of Canadians are considered literate.</li>
     </ul>
 </div>
 </setion>
 
+<section className='hypothesis-section mt2'>
 <div className="hypothesis-container">
-    <p className="t-large mb1 p1">Prediction: I believe that it is not only possible but advisable.</p>
+    <p className="t-large mb1 p1"><span className='t-large bold'>Prediction:</span> I believe I can radically change my career along with my mental and physical health.</p>
     <hr></hr>
     <p className='t-normal mt1 p1'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris malesuada augue id rutrum mollis. Morbi ut odio in tellus laoreet dignissim. Morbi ut erat in sem pharetra scelerisque maximus eu ante. Quisque accumsan urna pretium dolor iaculis finibus. Aliquam erat volutpat.</p>
 </div>
+</section>
 
+<section className='purpose-section mt2'>
 <div className="purpose-container">
-    <p className="t-large mb1 p1">Time to get the data!</p>
+    <p className="t-large bold mb1 p1">Follow me on my journey - Time to get the data!</p>
     <hr></hr>
     <p className='t-normal mt1 p1'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris malesuada augue id rutrum mollis. Morbi ut odio in tellus laoreet dignissim. Morbi ut erat in sem pharetra scelerisque maximus eu ante. Quisque accumsan urna pretium dolor iaculis finibus. Aliquam erat volutpat.</p>
 </div>
+</section>
     </div>
   )
 }
