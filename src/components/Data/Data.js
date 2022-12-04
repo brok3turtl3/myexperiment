6b4e514239ea44027788e0547231c7a8d3065db8
brok3turtl3@gmail.com <62915047+brok3turtl3@users.noreply.github.com>
2022-12-04 05:08:29 -0500

    Data page started and more fine tuning on Landing/Updates/Books pages

--- src/components/Data/Data.js
+++ src/components/Data/Data.js
@@ -3,7 +3,44 @@ import './data.css';
 
 const Data = () => {
   return (
-    <div>Data</div>
+    <div className="data-container">
+      <div className="data-overlay">
+        <div className="data-inner">
+
+      <div className="data-intro-container pt2">
+      <p className='data-intro pt2 t-medium'>Once a week I will post a summary of my week's wins and losses. I am sure there will be ups and downs and hopefully sharing them will help keep things in context for me and really show everyone else what the journey was actually like!</p>
+        </div>
+
+        <div className="data-card-container">
+        <div className='data-banner'>
+     <h1>Weight</h1> 
+
+    
+</div>
+<hr />
+<div className="data-info-section">
+  <div className="data-summary">
+    <ul>
+      <li>Blah blah</li>
+      <li>Boppity Boop</li>
+      <li>Abra Fabra</li>
+    </ul>
+  </div>
+  <div className="data-graph">
+    <img src="./images/Weight.png" alt="" />
+    {/* C:\Users\sean_\OneDrive\Desktop\myexperiment\src\components\Data\Weight.png
+    C:\Users\sean_\OneDrive\Desktop\myexperiment\public\images\data.css */}
+  </div>
+</div>
+        </div>
+        
+        
+        
+        
+        
+        </div>
+        </div>
+        </div>
   )
 }
 
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,14 @@ import './footer.css'
 
 const Footer = () => {
   return (
-    <div className='footer-container'><h1>Footer</h1></div>
+    <div className='footer-container'>
+      <span className=" copywrite t-medium beige pl1">&copy; 2022 Sean Kelly</span>
+    <span className='contacts t-medium beige pr1'>
+
+    <a href="https://twitter.com/brok3turtl3" target="_blank" rel="noreferrer"><img height="50px" className=" twitter beige" src="./images/twitter.png" alt="" />Twitter</a>
+
+    </span>
+    </div>
   )
 }
 
