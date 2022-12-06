0219e3b8086b431fbd8d8d754019dd5ac2abb2bb
Brok3Turtl3 <brok3turtl3@gmail.com>
2022-12-05 19:29:44 -0500

    Disable Blog nav item

--- src/components/HeaderNav/HeaderNav.js
+++ src/components/HeaderNav/HeaderNav.js
@@ -17,7 +17,7 @@ const HeaderNav = () => {
         <div className="navigation">
             <ul>
                 <li><Link to='/' >Home</Link></li>
-                <li><Link to='/blog'>Blog</Link></li>
+                {/* <li><Link to='/blog'>Blog</Link></li> */}
                 <li><Link to='/updates'>Updates</Link></li>
                 <li><Link to='/books' >Books</Link></li>
                 <li><Link to='/data'>Data</Link></li>
