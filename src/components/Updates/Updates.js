20b7f375cf37a1b9d53d98ea3cba092aa539b15c
brok3turtl3@gmail.com <62915047+brok3turtl3@users.noreply.github.com>
2022-12-12 19:28:29 -0500

    Weekly Update

--- src/components/Updates/Updates.js
+++ src/components/Updates/Updates.js
@@ -9,6 +9,48 @@ const Updates = () => {
       <div className="update-intro-container pt2">
         <p className='update-intro pt2 t-medium'>Once a week I will post a summary of my week's wins and losses. I am sure there will be ups and downs and hopefully sharing them will help keep things in context for me and really show everyone else what the journey was actually like!</p>
       </div>
+
+      <div className='update-card-container m2'>
+
+    <div className='update-banner'>
+     <p className='t-medium bold'>Week 13 Update</p>
+     <br /> 
+
+    <p className='t-medium bold pb1'>Dec 12th 2022</p>
+</div>
+
+<hr />
+
+
+    <div className="comments pt1">
+      <p className='t-medium bold'>They Better Be the Slow Kind!</p>
+      <br />
+      <p>So this week I started doing some light treadmill cardio. My early observation is that when the zombies come, if they are the fast kind, I am in serious trouble!</p>
+      <br />
+      <p>One of my goals is that after the 1 year mark I will be able to run 5km in 30min. In my first seesion a week ago I managed a distance of 2.19kms in 32min. I have a LONG way to go!</p>
+      <br />
+      <p>For the first 3 or 4 weeks I will do 30-45min sessions 3 or 4 times a week. After my body is used to the workouts I will adjust and start pushing towards the goal in earnest.</p>
+<br />
+      <p className='t-medium bold'>If You Code It, They Will Come.</p>
+      <br />
+      <p>Last week I brought this site and my portfolio page online. I am 95% satisfied with both but will continue to tweak and adjust as I get feedback. I now have 3 finsihed projects on the web and it feels great!</p> 
+      <br />
+      <p>The exciting thing that happened this week was how much traffic they have seen. Over time I expect this site to do fairly well and my family social has daily use but I did not expect the number of visits I have seen on my portfolio site. It crossed 250 unique visitors yesterday with almost 400 page views. Not huge numbers I am sure but pretty cool for my first steps into public!</p>
+      <br />
+      <p className='t-medium bold'>Christmas Is Coming...</p>
+      <br />
+      <p>This week I saw a slight weight increase. I can attribute it almost entirely to my first diet snap. A couple of drinks on Friday somehow led to 2 days of eating and drinking WAY too much... Pizza, wings, perrogies, oh my! It was more than a little disappointing stepping on the scale this afternoon but these things are going to happen. It will not be the last slip I am sure. I will go in to this week strong and get the gains (losses) back in no time!</p>
+      <br />
+      <p>This was probably a mild dry run for what Christmas will look like diet wise. With a full house of family here for 3 days there will be no shortage of bad (really good) food and drinks going around. I will try to stay super clean up to the 24th and I am hoping that when I come out the other side I can manage something in the mid 230's. Wish me luck!</p>
+      <br />
+      <p className='t-medium bold'>Do Comic Books Count?</p>
+      <br />
+      <p>Another of my goals that was ambitious. I am starting to feel like 50 books will be a real push. With the amount of coding that I want to be doing to move goal forward (started working on my next site this week) it only leaves so much time for reading. Even if I fall short though, like everything else I am doing, it will certainly be an improvement. Pretty soon my audiobooks will sound like they are on fast forward.</p>
+      <br />
+      <div className='center'>
+      <span className='t-medium bold'>See ya next week!</span>
+</div>
+</div></div>
       
       <div className='update-card-container m2'>
 
